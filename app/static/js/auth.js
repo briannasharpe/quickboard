4b282e0cce5270cb5d29933971f9ec0a91b3bc99
briannasharpe <36048465+briannasharpe@users.noreply.github.com>
2020-11-15 21:35:18 -0800

    Delete auth.js

--- app/static/js/auth.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("p").click(function () {
-        $("form").toggle(1000);
-    });
-});
-
-$('message a').click(function () {
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
